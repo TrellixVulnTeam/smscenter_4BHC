c1f533f5b3d1ce3ee9e1752761b206e5ac1bd5a6
Mukan Rajapov <mukhan350192@gmail.com>
2021-01-11 18:14:24 +0600

    Update

---
+++ app/Http/Controllers/SMSController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SMS;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SMSController extends Controller
+{
+    // for identification type=1
+    public function typeOne(Request $request){
+        $phone = $request->input('phone');
+        $code = $request->input('code');
+        $source = $request->input('source');
+
+        $result['success'] = false;
+        do{
+            if (!$phone){
+                $result['message'] = 'Не передан телефон';
+                break;
+            }
+            if (!$code){
+                $result['message'] = 'Не передан код';
+                break;
+            }
+            if (!$source){
+                $result['message'] = 'Не передан ссылка на сайт';
+                break;
+            }
+
+            DB::beginTransaction();
+            $login = env('SMS_CONSULT_LOGIN');
+            $password = env('SMS_CONSULT_PASSWORD');
+            $sender = env('SMS_CONSULT_SENDER');
+            $text = "Код для подтверждения на сайте $source $code";
+
+            $smsID = SMS::insertGetId([
+                'type' => 1,
+                'text' => $text,
+                'phone' => $phone,
+                'status' => 100,
+            ]);
+
+            $http = new Client;
+            try {
+                $response = $http->get('https://service.sms-consult.kz/get.ashx?', [
+                    'query' => [
+                        'login' => $login,
+                        'password' => $password,
+                        'id' => $smsID,
+                        'type' => 'message',
+                        'recipient' => $phone,
+                        'sender' => $sender,
+                        'text' => $text
+                    ],
+                ]);
+                $result = $response->getBody()->getContents();
+            } catch (BadResponseException $e) {
+
+                if ($e->getCode() == 400) {
+
+                    info('Something went wrong. Bad request' . $phone);
+                } elseif ($e->getCode() == 401) {
+
+                    info('Something went wrong. Bad request' . $phone);
+                }
+
+            }
+            DB::commit();
+            $result['success'] = true;
+        }while(false);
+        return response()->json($result);
+
+    }
+}
---
+++ database/migrations/2021_01_11_111700_create_s_m_s_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSMSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('type');
+            $table->string('status');
+            $table->string('phone');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('s_m_s');
+    }
+}
