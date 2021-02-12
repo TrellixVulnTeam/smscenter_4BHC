ccede015950d27884c56cc8cc22721b9424a0aac
Mukan Rajapov <mukhan350192@gmail.com>
2021-02-12 16:00:21 +0600

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-
+use Carbon\Carbon;
 class UserController extends Controller
 {
     public function index() {
