1458d4cbd449164cf839fa1690b70536875cf9c7
mukhan350192 <mukhan350129@gmail.com>
2021-04-26 15:00:02 +0600

    update

--- app/Console/Commands/CheckStatus.php
+++ app/Console/Commands/CheckStatus.php
@@ -101,12 +101,13 @@ class CheckStatus extends Command
                 if ($result == 'status=207'){
                     $t = 207;
                 }
+                $text = $s->text;
                 $update = SMS::where('id',$s->id)->update(['status' => $t,'updated_at' => Carbon::now()]);
                 $status_text = DB::table('sms_statuses')->select('description')->where('status',$t)->first();
                 if (isset($s) && isset($s->dealID)){
-                    $url = "https://icredit-crm.kz/api/webhock/responseDeal.php?dealID=$s->dealID&status_text='$status_text'&text='$s->text'";
+                    $url = "https://icredit-crm.kz/api/webhock/responseDeal.php?dealID=$s->dealID&status_text='$status_text'&text='$text'";
                 }else if (isset($s) && isset($s->leadID)){
-                    $url = "https://icredit-crm.kz/api/webhock/responseLead.php?leadID=$s->leadID&status_text='$status_text'&text='$s->text'";
+                    $url = "https://icredit-crm.kz/api/webhock/responseLead.php?leadID=$s->leadID&status_text='$status_text'&text='$text'";
                 }
             } catch (BadConversionException $e) {
                 info($e);
