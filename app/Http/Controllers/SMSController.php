496d75a4f26acb5833d228a60250cea81d3ea139
mukhan350192 <mukhan350129@gmail.com>
2021-04-26 14:20:40 +0600

    update

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -731,9 +731,13 @@ class SMSController extends Controller
                 }else if (isset($s) && isset($s->leadID)){
                     $url = "https://icredit-crm.kz/api/webhock/responseLead.php?leadID=$s->leadID&status_text='$status_text'&text=$s->text";
                 }
+                if (isset($url)){
+                    file_get_contents($url);
+                }
             } catch (BadConversionException $e) {
                 info($e);
             }
+            break;
         }
     }
 
